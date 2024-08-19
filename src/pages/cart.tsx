f0f7da53dd3cb76a07096f27485a88020da966d5
Paulo Borges <borgespaulo72@yahoo.com.br>
2024-08-19 16:37:09 -0300

    feat: estruturando o Projeto black_nextjs

---
+++ src/pages/cart.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Cart: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Nossos Produtos</title>
+      <meta name="description" content="Meu carrinho de compras"/>
+      <link rel="icon" href="/faviacon.ico" />
+    </Head>
+      <h1>
+        Carrinho
+      </h1>
+    </>
+  )
+}
+
+export default Cart
---
+++ src/pages/products.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Nossos Produtos</title>
+      <meta name="description" content="Conheça todos os nosso produtos"/>
+      <link rel="icon" href="/faviacon.ico" />
+    </Head>
+      <h1>
+        Nossos Produtos
+      </h1>
+    </>
+  )
+}
+
+export default Products
